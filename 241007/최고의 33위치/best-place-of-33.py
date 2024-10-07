cd1d849e056c51cbca24eb4b6a2810091315213d
YoungHwan <71543621+sherqy@users.noreply.github.com>
2024-10-07 19:55:31 +0900

    Title: 최고의 33위치, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241007/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,12 @@
+n = int(input())
+m = list(list(map(int, input().split())) for _ in range(n))
+c = []
+answer = 0
+
+for k in range(n - 2):
+    for i in range(n - 2):
+        for j in range(n - 2):
+            answer += m[j + k][i : i + 3].count(1)
+        c.append(answer)
+        answer = 0
+print(max(c))
