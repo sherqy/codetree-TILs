3b4213a33e478d36e566cdd287de6a516e3dbff5
YoungHwan <71543621+sherqy@users.noreply.github.com>
2024-10-11 02:09:03 +0900

    Title: 금 채굴하기, Time: 118ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241011/금 채굴하기/gold-mining.py
@@ -0,0 +1,46 @@
+import sys
+from itertools import combinations_with_replacement
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+f = list(list(map(int, input().split())) for _ in range(n))
+g = sum(list(sum(i) for i in f))
+k = 0
+answer = []
+total = 0
+dxdy = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+while k * k + (k + 1) * (k + 1) <= g * m:
+    direction = []
+
+    if k == 0:  #k가 0일 경우에는 확실히 답이 1
+        answer.append((0, 1))
+    else:       #k가 0이 아닐 경우에는 마름모에 따라 탐색
+        for i in combinations_with_replacement(dxdy, k):    #k값에 따라 마름모 크기 결정
+            x, y = 0, 0
+            direction.append((0, 0))
+            for j, l in i:
+                x += j
+                y += l
+            direction.append((x, y))
+        direction = list(set(direction))    #중복 제거
+        #마름모 크기에 따라 탐색하는 과정
+        for a in range(n):
+            for b in range(n):
+                maximum = 0
+                for x, y in direction:
+
+                    if a + x >= 0 and a + x < n and b + y >= 0 and b + y < n:
+                        if f[a + x][b + y] == 1:
+                            maximum += 1
+                total = maximum if maximum >= total else total
+        answer.append((k, total))
+    k += 1
+
+tri = 0
+
+for i, j in answer:
+    if j >= tri:
+        tri = j
+
+print(tri)
