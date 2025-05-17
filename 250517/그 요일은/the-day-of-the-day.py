5dce3820a89d26b1b91e435397dd3037176df5a1
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-17 16:11:37 +0900

    Title: 그 요일은, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250517/그 요일은/the-day-of-the-day.py
+++ 250517/그 요일은/the-day-of-the-day.py
@@ -4,16 +4,12 @@ A = input()
 # Please write your code here.
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
 cal1 = sum(month[:m1]) + d1 + days.index(A)
 cal2 = sum(month[:m2]) + d2
-ans = 1
+ans = 0
 
-while True:
+while cal2 >= cal1:
+    ans += 1
     cal1 += 7
-    if cal1 <= cal2:
-        ans += 1
-    else:
-        break
 
 print(ans)
