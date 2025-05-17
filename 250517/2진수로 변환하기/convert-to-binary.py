a0157389ce9e3dfc42b7abd8d33e747d2735be17
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-17 16:23:40 +0900

    Title: 2진수로 변환하기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250517/2진수로 변환하기/convert-to-binary.py
+++ 250517/2진수로 변환하기/convert-to-binary.py
@@ -3,8 +3,11 @@ n = int(input())
 # Please write your code here.
 di = []
 
-while n >= 0:
-    di.append(str(n % 2))
-    n = n // 2
+if n > 0:
+    while n > 0:
+        di.append(str(n % 2))
+        n = n // 2
+else:
+    di.append('0')
 
 print(''.join(reversed(di)))
