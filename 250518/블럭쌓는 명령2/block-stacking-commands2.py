fc36be7af827a10aad4ac991ad877b23269bb320
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-18 18:58:00 +0900

    Title: 블럭쌓는 명령2, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250518/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+commands = [tuple(map(int, input().split())) for _ in range(k)]
+
+# Please write your code here.
+block = [0] * n
+
+for i, j in commands:
+    for l in range(i, j + 1):
+        block[l] += 1
+
+print(max(block))
