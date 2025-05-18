92687e8c9001f63c22cd4ff6f214b17c84233780
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-19 05:11:17 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 111ms, Memory: 48MB, Status: Wrong Answer - Codetree

---
+++ 250519/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,43 @@
+x1, y1, x2, y2 = [0] * 2, [0] * 2, [0] * 2, [0] * 2
+x1[0], y1[0], x2[0], y2[0] = map(int, input().split())
+x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
+
+# Please write your code here.
+cross = [[0] * 2001 for _ in range(2002)]
+x_max = 0
+y_min = 0
+y_max = 0
+min_flag = 0
+max_flag = 0
+
+for i in range(2):
+    for j in range(x1[i] + 1000, x2[i] + 1000):
+        for k in range(y1[i] + 1000, y2[i] + 1000):
+            if i == 0:
+                cross[j][k] = 1
+            else:
+                cross[j][k] = 0
+
+for i in cross:
+    if sum(i) > x_max:
+        x_max = sum(i)
+
+for i in range(2001):
+    for j in range(2001):
+        if cross[i][j] == 1:
+            y_min = i
+            min_flag = 1
+            break
+    if min_flag == 1:
+        break
+
+for i in range(2000, -1, -1):
+    for j in range(2000, -1, -1):
+        if cross[i][j] == 1:
+            y_max = i
+            max_flag = 1
+            break
+    if max_flag == 1:
+        break
+
+print(x_max * (y_max - y_min + 1))
