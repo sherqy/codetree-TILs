9539166152af60f9fdedb2dd53ed73b7247c1fe9
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-20 18:13:52 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250520/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250520/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -2,12 +2,20 @@ N, K, P, T = map(int, input().split())
 handshakes = sorted([tuple(map(int, input().split())) for _ in range(T)], key = lambda x: x[0])
 count = [K] * N
 infected = [0] * N
-infected[P] = 1
+infected[P - 1] = 1
 
 for t, x, y in handshakes:
-    if infected[x - 1] or infected[y - 1] and (count[x - 1] and count[y - 1]):
-        infected[x - 1], infected[y - 1] = 1, 1
-        count[x - 1] -= 1
-        count[y - 1] -= 1
+    if infected[x - 1] and infected[y - 1]:
+        count[x - 1] -= (1 if count[x - 1] > 0 else 0)
+        count[y - 1] -= (1 if count[y - 1] > 0 else 0)
+    elif infected[x - 1] == 0 and infected[y - 1] == 0:
+        continue
+    else:
+        if infected[x - 1] and count[x - 1]:
+            count[x - 1] -= 1
+            infected[y - 1] = 1
+        elif infected[y - 1] and count[y - 1]:
+            count[y - 1] -= 1
+            infected[x - 1] = 1 
 
 print(*infected, sep = '')
