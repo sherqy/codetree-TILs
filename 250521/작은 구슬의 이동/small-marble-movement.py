6591721eaff407d4826ca96ba4bbe2ea424f2ebc
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-21 20:06:09 +0900

    Title: 작은 구슬의 이동, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250521/작은 구슬의 이동/small-marble-movement.py
+++ 250521/작은 구슬의 이동/small-marble-movement.py
@@ -3,7 +3,7 @@ r, c, d = input().split()
 r, c = int(r), int(c)
 mapper = {0 : 'U', 1 : 'R', 2 : 'D', 3 : 'L'}
 direction = [i for i, j in mapper.items() if j == d]
-drs, dcs = [1, 0, -1, 0], [0, 1, 0, -1]
+drs, dcs = [-1, 0, 1, 0], [0, 1, 0, -1]
 
 # Please write your code here.
 def in_range(x, y):
