0f6a75112c1bca3f1132a28e7f5ae8e319a1fc0f
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-21 21:45:45 +0900

    Title: 격자 위의 편안한 상태, Time: 115ms, Memory: 20MB, Status: Passed - Codetree

--- 250521/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 250521/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -4,16 +4,15 @@ dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
 grid = [[0] * n for _ in range(n)]
 
 def in_range(x, y):
-    return 1 <= x < n - 1 and 1 <= y < n - 1
+    return 0 <= x < n and 0 <= y < n
 
 for i, j in points:
     grid[i - 1][j - 1] = 1
     count = 0
 
-    if in_range(i - 1, j - 1):
-        for k, l in zip(dxs, dys):
-            if grid[i + k - 1][j + l - 1] == 1:
-                count += 1
+    for k, l in zip(dxs, dys):
+        if in_range(i + k - 1, j + l - 1) and grid[i + k - 1][j + l - 1] == 1:
+            count += 1
 
     if count == 3:
         print(1)
