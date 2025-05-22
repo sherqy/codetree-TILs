c74df4bf97963d3628d99c0419afeff5329c97fa
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-22 16:22:09 +0900

    Title: 거울에 레이저 쏘기 2, Time: 187ms, Memory: 49MB, Status: Runtime Error - Codetree

--- 250522/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 250522/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -3,49 +3,29 @@ grid = [list(input()) for _ in range(n)]
 k = int(input()) - 1
 
 dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
-direction = k // n
+direction = k // n + 1
 x, y = 0, 0
 ans = 0
 
-for i in range(1, k + 1):
+for i in range(0, k + 1):
     if i % n == 0:
         continue
     else:
         x += dxs[i // n]
         y += dys[i // n]
-
-cross = [[0, -1], [-1, 0], [1, 0], [0, 1]]
-
+    
 def in_range(nx, ny):
     return 0 <= nx < n and 0 <= ny < n
 
+def move(x, y, move_dir):
+    global dxs, dys
+    return x + dxs[move_dir], y + dys[move_dir], move_dir
+
 while in_range(x, y):
     ans += 1
-    if grid[x][y] == '/':
-        if direction == 0:
-            x, y = x + cross[0][0], y + cross[0][1]
-            direction = 1
-        elif direction == 1:
-            x, y = x + cross[2][0], y + cross[2][1]
-            direction = 0
-        elif direction == 2:
-            x, y = x + cross[3][0], y + cross[3][1]
-            direction = 3
-        else:
-            x, y = x + cross[1][0], y + cross[1][1]
-            direction = 2
+    if grid[x][y] == '\\':
+        x, y, direction = move(x, y, direction ^ 1)
     else:
-        if direction == 0:
-            x, y = x + cross[3][0], y + cross[3][1]
-            direction = 3
-        elif direction == 1:
-            x, y = x + cross[1][0], y + cross[1][1]
-            direction = 2
-        elif direction == 2:
-            x, y = x + cross[0][0], y + cross[0][1]
-            direction = 1
-        else:
-            x, y = x + cross[2][0], y + cross[2][1]
-            direction = 0
+        x, y, direction = move(x, y, 3 - direction)
 
 print(ans)
