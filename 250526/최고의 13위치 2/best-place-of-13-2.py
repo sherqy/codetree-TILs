9c3fe1941ef31691f58d562cfa0262b61a57065b
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-26 03:56:41 +0900

    Title: 최고의 13위치 2, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250526/최고의 13위치 2/best-place-of-13-2.py
+++ 250526/최고의 13위치 2/best-place-of-13-2.py
@@ -10,10 +10,9 @@ for i in range(n):
             for k in range(j + 3, n - 2):
                 sum2 = sum(arr[i][k : k + 3])
                 ans = max(ans, sum1 + sum2)
-        else:
-            for k in range(i + 1, n):
-                for l in range(n - 2):
-                    sum2 = sum(arr[k][l : l + 3])
-                    ans = max(ans, sum1 + sum2)
+        for k in range(i + 1, n):
+            for l in range(n - 2):
+                sum2 = sum(arr[k][l : l + 3])
+                ans = max(ans, sum1 + sum2)
 
 print(ans)
