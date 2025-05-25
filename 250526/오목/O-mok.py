cdbb7e53793020ba53918d02f707733149b8c758
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-26 03:30:53 +0900

    Title: 오목, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250526/오목/O-mok.py
+++ 250526/오목/O-mok.py
@@ -8,15 +8,15 @@ for i in range(0, 19):
         same_row, same_col, same_cross = 0, 0, 0
         
         if board[i][j] != 0:
-            if j <= 14 and sum(board[i][j : j + 5]) % 5 == 0:
+            if j <= 14 and board[i][j] == board[i][j + 1] == board[i][j + 2] == board[i][j + 3] == board[i][j + 4]:
                 print(board[i][j + 2])
                 print(i + 1, j + 3)
                 winner = board[i][j + 2]
-            if i <= 14 and (board[i][j] + board[i + 1][j] + board[i + 2][j] + board[i + 3][j] + board[i + 4][j]) % 5 == 0:
+            if i <= 14 and board[i][j] == board[i + 1][j] == board[i + 2][j] == board[i + 3][j] == board[i + 4][j]:
                 print(board[i + 2][j])
                 print(i + 3, j + 1)
                 winner = board[i + 2][j]
-            if i <= 14 and j <= 14 and (board[i][j] + board[i + 1][j + 1] + board[i + 2][j + 2] + board[i + 3][j + 3] + board[i + 4][j + 4]) % 5 == 0:
+            if i <= 14 and j <= 14 and board[i][j] == board[i + 1][j + 1] == board[i + 2][j + 2] == board[i + 3][j + 3] == board[i + 4][j + 4]:
                 print(board[i + 2][j + 2])
                 print(i + 3, j + 3)
                 winner = board[i + 2][j + 2]
