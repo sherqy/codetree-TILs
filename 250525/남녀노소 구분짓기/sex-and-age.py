0b7786661a06145c1e882912193f8a77fa822320
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-25 21:41:47 +0900

    Title: 남녀노소 구분짓기, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250525/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,10 @@
+s, a = map(int, input().split())
+
+if s == 0 and a >= 19:
+    print("MAN")
+elif s == 1 and a >= 19:
+    print("WOMAN")
+elif s == 0 and a < 19:
+    print("BOY")
+else:
+    print("GIRL")
