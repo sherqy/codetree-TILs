5edca24f139a843eeefe1775b80335caa3f61694
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-27 08:50:45 +0900

    Title: 오목, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250527/오목/O-mok.py
+++ 250527/오목/O-mok.py
@@ -1,27 +1,42 @@
 import sys
 
-board = [list(map(int, input().split())) for _ in range(19)]
-winner = 0
-dxs, dys = [1, 1, 0, -1, -1, -1, 0, 1], [0, 1, 1, 1, 0, -1, -1, -1]
+# 변수 선언 및 입력
+arr = [
+    list(map(int, input().split()))
+    for _ in range(19)
+]
 
+def in_range(x, y):
+    return 0 <= x and x < 19 and 0 <= y and y < 19
 
-def in_range(nx, ny):
-    return 0 <= nx < 19 and 0 <= ny < 19
-# Please write your code here.
+dxs, dys = [1, 1, 1, -1, -1, -1, 0, 0], [-1, 0, 1, -1, 0, 1, -1, 1]
 
-for i in range(0, 19):
-    for j in range(0, 19):
-        for k, l in zip(dxs, dys):
-            cnt = []
-            for m in range(5):
-                if in_range(i + k * m, j + l * m):
-                    cnt.append(board[i + k * m][j + l * m])
+# 모든 좌표에서 다 확인해봅니다.
+for i in range(19):
+	# 격자를 벗어나지 않을 범위로만 잡습니다.
+	for j in range(19):
+		
+		if arr[i][j] == 0:
+			continue
+		
+		for dx, dy in zip(dxs, dys):
+			curt = 1
+			curx = i
+			cury = j
+			while True:
+				nx = curx + dx
+				ny = cury + dy
+				if in_range(nx, ny) == False:
+					break
+				if arr[nx][ny] != arr[i][j]:
+					break
+				curt += 1
+				curx = nx
+				cury = ny
+			
+			if curt == 5:
+				print(arr[i][j])
+				print(i + 2 * dx + 1, j + 2 * dy + 1)
+				sys.exit()
 
-            if cnt.count(1) == 5 or cnt.count(2) == 5:
-                winner = cnt[0]
-                print(cnt[0])
-                print(i + k * 2 + 1, j + l * 2 + 1)
-                sys.exit()
-
-if winner == 0:
-    print(winner)
+print(0)
