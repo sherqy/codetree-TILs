df7d6fafa14e523cd211e87be901c9397c060064
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-27 09:11:58 +0900

    Title: G or H 3, Time: 72ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250527/G or H 3/G-or-H-3.py
+++ 250527/G or H 3/G-or-H-3.py
@@ -2,13 +2,15 @@ n, k = map(int, input().split())
 x = [''] * 10001
 c = []
 ans = 0
+max_pos = 0
 
 for _ in range(1, n + 1):
     pos, char = input().split()
+    max_pos = max(pos, max_pos)
     x[int(pos)] = char
 
 # Please write your code here.
-for i in range(1, 10001 - k):
+for i in range(1, max_pos - k):
     cnt = 0
 
     for j in range(i, i + k + 1):
