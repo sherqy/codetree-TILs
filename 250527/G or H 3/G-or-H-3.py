ed5d119063247ba05d8559cbae462a3151d9fe4f
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-27 09:24:55 +0900

    Title: G or H 3, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250527/G or H 3/G-or-H-3.py
+++ 250527/G or H 3/G-or-H-3.py
@@ -1,24 +1,21 @@
 n, k = map(int, input().split())
-x = [''] * 10001
-c = []
+x = [0] * 10001
 ans = 0
 max_pos = 0
 
 for _ in range(1, n + 1):
     pos, char = input().split()
     max_pos = max(int(pos), max_pos)
-    x[int(pos)] = char
+    
+    if char == 'G':
+        x[int(pos)] = 1
+    elif char == 'H':
+        x[int(pos)] = 2
+    else:
+        x[int(pos)] = 0
 
 # Please write your code here.
 for i in range(1, max_pos - k + 1):
-    cnt = 0
-
-    for j in range(i, i + k + 1):
-        if x[j] == 'G':
-            cnt += 1
-        elif x[j] == 'H':
-            cnt += 2
-
-    ans = max(cnt, ans)
+    ans = max(sum(x[i : i + k + 1]), ans)
 
 print(ans)
