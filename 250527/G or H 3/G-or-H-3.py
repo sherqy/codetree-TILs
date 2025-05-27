b6c7e05c58d80a058c923fd60d706dc8c6d81c86
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-27 09:11:14 +0900

    Title: G or H 3, Time: 290ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250527/G or H 3/G-or-H-3.py
@@ -0,0 +1,22 @@
+n, k = map(int, input().split())
+x = [''] * 10001
+c = []
+ans = 0
+
+for _ in range(1, n + 1):
+    pos, char = input().split()
+    x[int(pos)] = char
+
+# Please write your code here.
+for i in range(1, 10001 - k):
+    cnt = 0
+
+    for j in range(i, i + k + 1):
+        if x[j] == 'G':
+            cnt += 1
+        elif x[j] == 'H':
+            cnt += 2
+
+    ans = max(cnt, ans)
+
+print(ans)
