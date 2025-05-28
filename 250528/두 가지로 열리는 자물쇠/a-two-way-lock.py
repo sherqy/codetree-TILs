1d6b8e95aee7bfaa2bf1c2bad5581b0b63c91b55
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-28 22:30:34 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 72ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250528/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 250528/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -3,21 +3,17 @@ a1, b1, c1 = map(int, input().split())
 a2, b2, c2 = map(int, input().split())
 ans = 0
 
-def in_dial(m, n):
-    if abs(m - n) <= 2 or 1 <= N - abs(m - n) <= 2:
-        return True
-
-def unlock(x, y, z):
-    if in_dial(a1, x) and in_dial(b1, y) and in_dial(c1, z):
-        return True
-    elif in_dial(a2, x) and in_dial(b2, y) and in_dial(c2, z):
-        return True
-
 # Please write your code here.
 for i in range(1, N + 1):
     for j in range(1, N + 1):
         for k in range(1, N + 1):
-            if unlock(i, j, k):
+            if (abs(a1 - i) <= 2 or abs(a1 - i) >= n - 2) and (abs(b1 - j) <= 2 or abs(b1 - j) >= n - 2) and \
+               (abs(c1 - k) <= 2 or abs(c1 - k) >= n - 2):
+                ans += 1
+			
+			# 모든 자리가 주어진 두 번째 조합과의 거리가 2 이내인지 확인합니다.
+            elif (abs(a2 - i) <= 2 or abs(a2 - i) >= n - 2) and (abs(b2 - j) <= 2 or abs(b2 - j) >= n - 2) and \
+               (abs(c2 - k) <= 2 or abs(c2 - k) >= n - 2):
                 ans += 1
 
 print(ans)
