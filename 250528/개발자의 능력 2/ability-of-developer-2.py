693130e23d93e7d93d8b847874fb775cbe0df6ec
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-28 22:45:07 +0900

    Title: 개발자의 능력 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250528/개발자의 능력 2/ability-of-developer-2.py
@@ -0,0 +1,19 @@
+import sys
+
+ability = list(map(int, input().split()))
+ans = sys.maxsize
+
+# Please write your code here.
+for i in range(5):
+    for j in range(i + 1, 6):
+
+        sum1 = ability[i] + ability[j]
+
+        for k in range(5):
+            for l in range(k + 1, 6):
+                if i != k and j != l:
+                    sum2 = ability[k] + ability[l]
+                    sum3 = sum(ability) - sum1 - sum2
+                    ans = min(max(sum1, sum2, sum3) - min(sum1, sum2, sum3), ans)
+
+print(ans)
