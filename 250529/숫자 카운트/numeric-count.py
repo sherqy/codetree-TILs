54025ad5965d98aaa8c99fd107fd84cc2856932d
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-29 11:05:42 +0900

    Title: 숫자 카운트, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250529/숫자 카운트/numeric-count.py
+++ 250529/숫자 카운트/numeric-count.py
@@ -1,36 +1,41 @@
 n = int(input())
-arr = [tuple(map(int, input().split())) for _ in range(n)]
+a, b, c = [], [], []
 ans = 0
+for _ in range(n):
+    num, cnt1, cnt2 = map(int, input().split())
+    a.append(num)
+    b.append(cnt1)
+    c.append(cnt2)
 
 for i in range(1, 10):
     for j in range(1, 10):
         for k in range(1, 10):
-            
             if i == j or j == k or k == i:
                 continue
-    
-            suc = True
-            
-            for b, cnt1, cnt2 in arr:
-                x = [b // 100, (b % 100) // 10, b % 10]
-                y, z = 0, 0
-                
-                if i == x[0]:
-                    y += 1
-                if i == x[1] or i == x[2]:
-                    z += 1
-                if j == x[1]:
-                    y += 1
-                if j == x[0] or j == x[2]:
-                    z += 1
-                if k == x[2]:
-                    y += 1
-                if k == x[0] or k == x[1]:
-                    z += 1
+            else:
+                suc = True
 
-                if y != cnt1 or z != cnt2:
-                    suc = False
-                    break
-            if suc:
-                ans += 1
+                for l, m, n in zip(a, b, c):
+                    x, y = 0, 0
+                    z = [l // 100, (l % 100) // 10, l % 10]
+                    
+                    if i == z[0]:
+                        x += 1
+                    if j == z[1]:
+                        x += 1
+                    if k == z[2]:
+                        x += 1
+                    if i == z[1] or i == z[2]:
+                        y += 1
+                    if j == z[0] or j == z[2]:
+                        y += 1
+                    if k == z[0] or k == z[1]:
+                        y += 1
+
+                    if x != m or y != n:
+                        suc = False
+                        break
+                if suc:
+                    ans += 1
+
 print(ans)
