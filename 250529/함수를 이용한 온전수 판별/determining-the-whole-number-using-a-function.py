d161045355621643691aac73d9e3a6adc43da796
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-29 11:38:32 +0900

    Title: 함수를 이용한 온전수 판별, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250529/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
+++ 250529/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -3,9 +3,14 @@ a, b = map(int, input().split())
 def is_number(x, y):
     ans = 0
     for i in range(x, y + 1):
-        if i % 2 != 0 and i % 10 != 5 and i % 3 != 0 or i % 9 == 0:
+        if i % 2 == 0:
+            ans += 0
+        elif i % 10 == 5:
+            ans += 0
+        elif i % 3 == 0 and i % 9 != 0:
+            ans += 0
+        else:
             ans += 1
-
     return ans
 
 print(is_number(a, b))
