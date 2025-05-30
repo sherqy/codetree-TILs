081dfdf39afc5559b7df8a6e440c145a148db6bc
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-05-30 17:53:21 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 1009ms, Memory: 24MB, Status: Time Limit Exceed - Codetree

--- 250530/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 250530/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -5,6 +5,6 @@ def max_val(a):
     if len(a) == 1:
         return a[0]
     
-    return max_val(a[0 : len(a) - 1]) if a[-1] < max_val(a[0 : len(a) - 1]) else a[-1]
+    return max_val(a[0 : len(a) - 1]) if a[-1] <= max_val(a[0 : len(a) - 1]) else a[-1]
 
 print(max_val(arr))
