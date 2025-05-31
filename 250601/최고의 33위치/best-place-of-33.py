be442c3346c3c2e46d1471d83102223e86a9d054
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-06-01 00:58:47 +0900

    Title: 최고의 33위치, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250601/최고의 33위치/best-place-of-33.py
+++ 250601/최고의 33위치/best-place-of-33.py
@@ -6,7 +6,7 @@ for i in range(n - 2):
     for j in range(n - 2):
         cnt = 0
         
-        for k in range(3):
+        for k in range(i, i + 3):
             cnt += sum(grid[k][j : j + 3])
 
         ans = max(cnt, ans)
