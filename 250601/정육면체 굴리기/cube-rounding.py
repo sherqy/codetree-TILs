c3e9edc9315e384968d0a764307e88783d6353d9
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-06-01 14:19:15 +0900

    Title: 정육면체 굴리기, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 250601/정육면체 굴리기/cube-rounding.py
+++ 250601/정육면체 굴리기/cube-rounding.py
@@ -4,7 +4,7 @@ com = list(map(int, input().split()))
 
 dxs, dys = [0, 0, -1, 1], [1, -1, 0, 0]
 cube = [0] * 7
-up, front, right = 1, 2, 3
+up, front, right, bottom = 1, 2, 3, 6
 
 def in_range(nx, ny):
     return 0 <= nx < n and 0 <= ny < m
@@ -14,18 +14,19 @@ for i in com:
         x, y = x + dxs[i - 1], y + dys[i - 1]
 
         if i - 1 == 0:
-            cube[right], grid[x][y] = grid[x][y], cube[right]
             up, front, right = 7 - right, front, up
-            print(cube[up])
         elif i - 1 == 1:
-            cube[7 - right], grid[x][y] = grid[x][y], cube[7 - right]
             up, front, right = right, front, 7 - up
-            print(cube[up])
         elif i - 1 == 2:
-            cube[7 - front], grid[x][y] = grid[x][y], cube[7 - front]
             up, front, right = front, 7 - up, right
-            print(cube[up])
         elif i - 1 == 3:
-            cube[front], grid[x][y] = grid[x][y], cube[front]
             up, front, right = 7 - front, up, right
-            print(cube[up])
+
+        bottom = 7 - up
+
+        if grid[x][y] == 0:
+            grid[x][y] = cube[bottom]
+        else:
+            grid[x][y], cube[bottom] = 0, grid[x][y]
+
+        print(cube[up])
