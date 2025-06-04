3ed2973da69362309a70c1662276110a8a31bf66
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-06-04 17:17:06 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250604/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 250604/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,8 +1,11 @@
 n1, n2 = map(int, input().split())
-a = list(map(str, input().split()))
-b = list(map(str, input().split()))
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
 
 def com(x, y):
-    return ''.join(y) in ''.join(x)
+    for i in range(n1 - n2):
+        if x[i : i + n2] == y:
+            return True
+    return False
 
 print("Yes" if com(a, b) else "No")
