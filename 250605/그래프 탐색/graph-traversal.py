9b57a5f3b84544041ac5c2570cee0413fc34d3f3
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-06-05 16:21:01 +0900

    Title: 그래프 탐색, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250605/그래프 탐색/graph-traversal.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+grid = [[0] * n for _ in range(n)]
+visited = [False] * n
+
+for i, j in edges:
+    grid[i - 1][j - 1] = 1
+    grid[j - 1][i - 1] = 1
+
+def dfs(x):
+    a = 0
+    for curr in range(n):
+        if grid[x][curr] == 1 and not visited[curr]:
+            visited[curr] = True
+            a = a + 1 + dfs(curr)
+    return a
+
+print(dfs(0) - 1)
