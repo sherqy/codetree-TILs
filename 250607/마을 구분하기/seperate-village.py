d4518d1d03771fd7cd41b55f1b3c3613bb18f03d
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-06-07 19:25:45 +0900

    Title: 마을 구분하기, Time: 62ms, Memory: 17MB, Status: Passed - Codetree

--- 250607/마을 구분하기/seperate-village.py
+++ 250607/마을 구분하기/seperate-village.py
@@ -1,35 +1,29 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 visited = [[False] * n for _ in range(n)]
-cnt = 0
 ans = []
 
 def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
+    return 0 <= x < n and 0 <= y < n and visited[x][y] == False and grid[x][y] == 1
 
 def dfs(x, y):
-    global cnt
     dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
     visited[x][y] = True
-    cnt += 1
+    count = 1
 
     for nx, ny in zip(dxs, dys):
         new_x, new_y = x + nx, y + ny
 
-        if in_range(new_x, new_y) and visited[new_x][new_y] == False and grid[new_x][new_y] == 1:
-            visited[new_x][new_y] = True
-            dfs(new_x, new_y)
+        if in_range(new_x, new_y):
+            count += dfs(new_x, new_y)
+
+    return count
 
 
 for i in range(n):
     for j in range(n):
-        if visited[i][j] == False and grid[i][j] == 1:
-            dfs(i, j)
-
-            if cnt != 0:
-                ans.append(cnt)
-
-            cnt = 0
+        if in_range(i, j):
+            ans.append(dfs(i, j))
 
 print(len(ans))
 print(*sorted(ans), sep = '\n')
