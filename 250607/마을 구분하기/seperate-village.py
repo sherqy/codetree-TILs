2887443c3bf5de90cad2a385e2b281a9f8936862
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-06-07 19:11:47 +0900

    Title: 마을 구분하기, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 250607/마을 구분하기/seperate-village.py
+++ 250607/마을 구분하기/seperate-village.py
@@ -1,33 +1,43 @@
 n = int(input())
-grid = [list(map(int, input().split())) for _ in range(n)]
-visited = [[False] * n for _ in range(n)]
-cnt = 0
-ans = []
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+visited = [[0]*n for _ in range(n)]
 
-def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
+cnt = 0
+people = []
 
 def dfs(x, y):
     global cnt
-    dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
+    visited[x][y] = 1
+    cnt += 1
 
-    for nx, ny in zip(dxs, dys):
-        new_x, new_y = x + nx, y + ny
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
 
-        if in_range(new_x, new_y) and visited[new_x][new_y] == False and grid[new_x][new_y] == 1:
-            visited[new_x][new_y] = True
-            cnt += 1
+    for dx, dy in zip(dxs, dys):
+        new_x, new_y = x + dx, y + dy
+        if can_go(new_x, new_y):
             dfs(new_x, new_y)
+
+def can_go(x, y):
+    if x >= n or y >= n:
+        return False
+    if visited[x][y] or grid[x][y] == 0:
+        return False
+    if x < 0 or y < 0: 
+        return False
+    return True
 
 
 for i in range(n):
     for j in range(n):
-        dfs(i, j)
+        if can_go(i, j):
+            cnt = 0
+            dfs(i, j)
+            people.append(cnt)
 
-        if cnt != 0:
-            ans.append(cnt)
-
-        cnt = 0
-
-print(len(ans))
-print(*sorted(ans), sep = '\n')
+people.sort()
+print(len(people))
+for i in range(len(people)):
+    print(people[i])
