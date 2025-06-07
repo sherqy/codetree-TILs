6f458bc42a096141553562dfec5fcd11fe241759
YoungHwan <71543621+sherqy@users.noreply.github.com>
2025-06-07 15:11:58 +0900

    Title: 배수의 개수, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250607/배수의 개수/number-of-multipliers.py
+++ 250607/배수의 개수/number-of-multipliers.py
@@ -1,9 +1,12 @@
-ans = 0
+ans_3 = 0
+ans_5 = 0
 
 for _ in range(10):
     a = int(input())
 
-    if a % 3 == 0 or a % 5 == 0:
-        ans += 1
+    if a % 3 == 0:
+        ans_3 += 1
+    if a % 5 == 0:
+        ans_5 += 1
 
-print(ans)
+print(ans_3, ans_5)
